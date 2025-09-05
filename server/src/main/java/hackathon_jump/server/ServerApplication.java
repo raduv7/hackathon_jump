7395100fbb8b21796d10b74c29482b1fc5b359d9
Radu Vaida <111689573+raduv7@users.noreply.github.com>
2025-09-05 17:01:05 +0300

    server setup

---
+++ server/src/main/java/hackathon_jump/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package hackathon_jump.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
