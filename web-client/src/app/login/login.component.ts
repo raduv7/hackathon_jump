39ec60ab6fc487b4fde829c550b7537d51b991be
Radu Vaida <111689573+raduv7@users.noreply.github.com>
2025-09-05 18:33:00 +0300

    implement login add logo add background

---
+++ web-client/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { API_BASE_URL } from '../security.config';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  googleAuthUrl = `${API_BASE_URL}/oauth2/authorization/google`;
+}
