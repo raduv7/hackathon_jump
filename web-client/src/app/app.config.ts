27255875b728feccbc9f436e49df863f9f187bfb
Radu Vaida <111689573+raduv7@users.noreply.github.com>
2025-09-05 17:32:48 +0300

    connect to server

--- web-client/src/app/app.config.ts
+++ web-client/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { API_BASE_URL } from './security.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([
+      (req, next) => {
+        const isAbsolute = /^https?:\/\//i.test(req.url);
+        const url = isAbsolute ? req.url : `${API_BASE_URL}${req.url}`;
+        return next(req.clone({ url }));
+      }
+    ]))
+  ]
 };
